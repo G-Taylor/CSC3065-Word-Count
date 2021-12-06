01c11d5de8cf1520e5bd3d0b3cc27c1938e8f212
40234272 <gtaylor18@qub.ac.uk>
2021-12-06 14:34:24 +0000

    added validation to PHP function

--- src/index.php
+++ src/index.php
@@ -11,10 +11,18 @@ $output = array(
 
 $t = $_REQUEST['text'];
 
-$answer=wordcount($t);
+if($t === NULL) {
+	$output["error"] = true;
+	$output["string"] = "Incorrect Parameters Entered";
+} elseif ($t === "") {
+	$output["error"] = true;
+	$output["string"] = "No Text Entered";
+} else {
+	$answer=wordcount($t);
 
-$output['string']="Contains ".$answer." words";
-$output['answer']=$answer;
+	$output['string']= "Contains ".$answer." words";
+	$output['answer']= $answer;
+}
 
 echo json_encode($output);
 exit();
